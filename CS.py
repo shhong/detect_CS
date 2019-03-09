e4b47c43394f711ab0415d56cf6e49972b24046f
jobellet <jobellet@gmail.com>
2019-03-09 12:30:28 +0100

    did not work

--- CS.py
+++ CS.py
@@ -1,24 +1,11 @@
-Path_to_UnEye_folder = '/gdrive/My Drive/uneye/'
-# go to the uneye folder
-import os
-os.chdir(Path_to_UnEye_folder)
 
 ## install dependencies
 
 
-# to do deep learning
-# http://pytorch.org/
-from os.path import exists
-from wheel.pep425tags import get_abbr_impl, get_impl_ver, get_abi_tag
-platform = '{}{}-{}'.format(get_abbr_impl(), get_impl_ver(), get_abi_tag())
-cuda_output = !ldconfig -p|grep cudart.so|sed -e 's/.*\.\([0-9]*\)\.\([0-9]*\)$/cu\1\2/'
-accelerator = cuda_output[0] if exists('/dev/nvidia0') else 'cpu'
 
-!pip install -q http://download.pytorch.org/whl/{accelerator}/torch-0.4.1-{platform}-linux_x86_64.whl torchvision
 import torch
 
 # to read .mat files
-!pip install mat4py
 import mat4py
 
 # to read .pkl files
@@ -32,12 +19,11 @@ import uneye
 
 # to get list of files
 from glob import glob as dir
-# to do dimensionality reduction
 
+# to do dimensionality reduction
 import umap
 
 # to do unsupervised clustering
-#from sklearn.cluster import DBSCAN
 import hdbscan
 
 # to plot things
