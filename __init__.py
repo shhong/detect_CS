9051249feb43440a2c3d54acde0a49bea6d156ed
jobellet <jobellet@gmail.com>
2019-03-10 22:10:24 +0100

    add functions

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
+from .CS import get_field_mat
+from .CS import get_field_pkl
+from .CS import list2array
+from .CS import norm_LFP
+from .CS import load_data
+from .CS import save_data
 from .CS import detect_CS
